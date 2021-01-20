ad9ad4261358396e06d77f9750a1f4bff0c8b2c9
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 16:05:26 -0600

    Added toolbar for example app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import logo from './logo.svg';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="h6" color="inherit">
+            SHOPPINGLIST
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
