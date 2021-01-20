de4e63b99a466a45c49bddee21819b3155d7bad5
Arturo Suárez <masuareb@hotmail.com>
2021-01-20 16:54:23 -0600

    Add List and ListItem components

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import './App.css';
 import AddItem from './AddItem';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function App() {
   const [items, setItems] = React.useState([]);
   const addItem = (item) => {
     setItems([item, ...items]);
   }
+  const listItems = items.map((item, index) => 
+    <ListItem>
+      <ListItemText primary={item.product} secondary={item.amount}>
+      </ListItemText>
+    </ListItem>);
   return (
     <div className="App">
       <AppBar position="static" color="default">
@@ -20,6 +28,7 @@ function App() {
         </Toolbar>
       </AppBar>
       <AddItem addItem={addItem} />
+      <List>{listItems}</List>
     </div>
   );
 }
